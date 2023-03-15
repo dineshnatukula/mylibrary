ca396eb5f2937312907c2868d86db6a71447527a
Dinesh Venkata Siva Sankar Natukula <dinesh.natukula@gmail.com>
2023-03-15 23:32:10 +0530

    ad

---
+++ arthmetic.go
@@ -0,0 +1,15 @@
+package mylibrary
+
+/*
+This function takes two integer params and return an addition of two integers as int
+*/
+func Add(a, b int) int {
+	return a + b
+}
+
+/*
+This function takes two integer params and return an subtraction of two integers as int
+*/
+func Sub(a, b int) int {
+	return a - b
+}
