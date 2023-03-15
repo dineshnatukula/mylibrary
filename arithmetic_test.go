be65c15fa108c5a06d1b406f8d81c0ef72133f69
Dinesh Venkata Siva Sankar Natukula <dinesh.natukula@gmail.com>
2023-03-15 23:18:51 +0530

    my library

---
+++ arithmetic_test.go
@@ -0,0 +1,40 @@
+package mylibrary_test
+
+import (
+	math "mylibrary/math"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	result := math.Add(2, 3)
+	if result != 5 {
+		t.Errorf("Add(2, 3) returned %d, expected %d", result, 5)
+	}
+
+	result = math.Add(2, 8)
+	if result != 10 {
+		t.Errorf("Add(2, 3) returned %d, expected %d", result, 5)
+	}
+
+	result = math.Add(2, -3)
+	if result != -1 {
+		t.Errorf("Add(2, 3) returned %d, expected %d", result, 5)
+	}
+}
+
+func TestSub(t *testing.T) {
+	result := math.Sub(2, 3)
+	if result != -1 {
+		t.Errorf("Sub(2, 3) returned %d, expected %d", result, 5)
+	}
+
+	result = math.Sub(2, 8)
+	if result != -6 {
+		t.Errorf("Sub(2, 3) returned %d, expected %d", result, 5)
+	}
+
+	result = math.Sub(2, -3)
+	if result != 5 {
+		t.Errorf("Sub(2, 3) returned %d, expected %d", result, 5)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module mylibrary
+
+go 1.18
